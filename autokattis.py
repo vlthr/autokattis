386970fb824e75a7c8c1bd813fb2996d55cc0f44
Valthor Halldorsson <halldorsson.v@gmail.com>
2017-05-21 23:35:15 +0200

    basic sample downloading and autotesting

--- autokattis.py
+++ autokattis.py
@@ -1,28 +1,93 @@
-from bs4 import BeautifulSoup
+import glob
+import subprocess
+import difflib
+import colorama
+from colorama import Fore, Back, Style
 import requests
+import os
+import zipfile
+import io
 import sys
 import itertools
+from docopt import docopt
+
+doc = """Autokattis
+
+Usage:
+    autokattis.py init [--program=<NAME>] 
+    autokattis.py test [--program=<NAME>] <PROGRAM>
+    autokattis.py (-h | --help)
+
+Options:
+    (-p | --problem) <NAME>       The name of the kattis problem
+                                (if unspecified, uses directory name)
+
+Subcommands:
+    init                        Download samples for the given problem
+    test <PROGRAM>                       Run PROGRAM on all samples
+"""
+SAMPLES_DIR = 'samples'
 
 
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return itertools.zip_longest(*args, fillvalue=fillvalue)
+def download_samples(problem, directory=SAMPLES_DIR):
+    r = requests.get(f'https://open.kattis.com/problems/{problem}/file/statement/samples.zip')
+    if r.status_code != 200:
+        raise IOError(f"Kattis request for problem '{problem}' unsuccessful with error code: {r.status_code}")
+    file = zipfile.ZipFile(io.BytesIO(r.content))
+    file.extractall(directory)
+
+
+def current_dir_name():
+    return os.path.basename(os.getcwd())
+
+def c(msg, color):
+    return color+msg+Style.RESET_ALL
+
+def test(program, input_file, expected_file):
+    inp = open(input_file).read()
+    expected = open(expected_file).read()
+    outp = run(program, inp)
+    sys.stdout.write(c(input_file, Style.DIM))
+    sys.stdout.write("... ")
+    diff = [d for d in difflib.unified_diff(outp.split('\n'), expected.split('\n'))]
+    if diff:
+        print(c("Fail!", Fore.RED))
+        for line in diff:
+            sys.stdout.write('\t'+line)
+    else:
+        print(c("Success!", Fore.GREEN))
+
+
+def run(program, inp):
+    p = subprocess.Popen(program.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    outp, err = p.communicate(inp.encode('utf-8'))
+    if err:
+        raise ChildProcessError(err)
+    return outp.decode('utf-8')
+
+
+def run_tests(program, input_dir=SAMPLES_DIR):
+    for inp in glob.glob(f'{input_dir}/*.in'):
+        outp = inp.replace('.in', '.ans')
+        test(program, inp, outp)
+
+
+def samples_dir_exists():
+    return os.path.isdir(SAMPLES_DIR)
 
 
 def main():
-    problem = sys.argv[1]
-    r = requests.get(f'https://open.kattis.com/problems/{problem}')
-    if r.status_code != 200:
-        print(f"Error: Kattis request for problem '{problem}' unsuccessful with error code: {r.status_code}")
+    colorama.init()
+    args = docopt(doc)
+    print(args)
+    if args['-h']:
+        print(doc)
         return
-    html = r.text
-    soup = BeautifulSoup(html, "lxml")
-    samples = soup.select('.sample pre')
-    for inp, outp in grouper(samples, 2):
-        print(inp.text.strip())
-        print(outp.text.strip())
+    problem = args['--program'] if args['--program'] else current_dir_name()
+    if args['init']:
+        download_samples(problem)
+    elif args['test']:
+        run_tests(args['<PROGRAM>'])
 
 
 if __name__ == "__main__":
